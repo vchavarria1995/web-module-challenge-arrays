db016e83db5ab7fba7e1c9983252a425ea62f625
vchavarria1995 <vchavarria1995@gmail.com>
2021-06-09 17:25:05 -0500

    finished task 5, last commit as suppose to be 4

--- index.js
+++ index.js
@@ -122,9 +122,10 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(array, index){
+    return array[index];
 }
+console.log ('task 5', getFlavorByIndex(originalFlavors, 2));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
